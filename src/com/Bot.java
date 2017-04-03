e87f97002b3bb132719eaffe04a2b707b7e2ee0a
ClaudePlos <claude-plos@o2.pl>
2017-04-03 14:56:15 +0200

    bot do something

--- src/com/Bot.java
+++ src/com/Bot.java
@@ -1,11 +1,17 @@
 package com;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class Bot extends JFrame {
 	
 	private JTextField txtEnter = new JTextField();
+	
+	private JTextArea txtChat = new JTextArea();
 	
 	public Bot() {
 		
@@ -17,10 +23,48 @@ public class Bot extends JFrame {
 		this.setTitle("AI");
 		
 		txtEnter.setLocation(2, 540);
-		txtEnter.setSize(590,30);
+		txtEnter.setSize(590, 30);
+		
+		txtEnter.addActionListener( new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String uText = txtEnter.getText();			
+				txtChat.append("You: " + uText + "\n");
+				
+				if (uText.contains("hi")){
+					botSay("Hello there!");
+				}
+				else{
+					int decider = (int) (Math.random()*3+1);
+					
+					if ( decider == 1 ){
+						botSay("I didn't get that");
+					}
+					else if (  decider == 2 ){
+						botSay("Please rephrase that");
+					}
+					else if (  decider == 3 ){
+						botSay("???");
+					}
+					
+				}
+				
+				txtEnter.setText("");			
+			}
+		});
+		
+		txtChat.setLocation(15, 5);
+		txtChat.setSize(560, 510);
+		txtChat.setEditable(false);
 		
 		this.add(txtEnter);
+		this.add(txtChat);
 		
+	}
+	
+	public void botSay( String s ){
+		txtChat.append("AI: " + s + "\n");
 	}
 	
 	public static void main(String[] args) {
